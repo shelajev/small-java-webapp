ccc28b023848367fd417fddc637719492c6fd63c
Oleg Shelajev <shelajev@gmail.com>
2016-07-22 14:12:32 +0300

    Initial commit from start.spring.io

---
+++ src/main/java/org/zeroturnaround/SmallJavaWebappApplication.java
@@ -0,0 +1,12 @@
+package org.zeroturnaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallJavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallJavaWebappApplication.class, args);
+	}
+}
